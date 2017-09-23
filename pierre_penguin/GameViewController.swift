12990b0b9824436f073e0361a958ad5fa5cd9409
cdrainxv <cdrainxv@users.noreply.github.com>
2017-09-22 21:09:45 -0600

    setup scene layout (landscape) and sizing (fill screen)

--- pierre_penguin/GameViewController.swift
+++ pierre_penguin/GameViewController.swift
@@ -11,44 +11,41 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
+            
             if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
+                // Set the scale mode fit the window:
                 scene.scaleMode = .aspectFill
-                
-                // Present the scene
+                // Size our scene to fit the view exactly:
+                scene.size = view.bounds.size
+                // Show the new scene:
                 view.presentScene(scene)
             }
             
             view.ignoresSiblingOrder = true
-            
             view.showsFPS = true
             view.showsNodeCount = true
         }
     }
-
+    
     override var shouldAutorotate: Bool {
         return true
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        return .landscape
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
