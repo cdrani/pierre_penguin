032d409ef7e26e0885c58178196a36ea64c09445
cdrainxv <cdrainxv@users.noreply.github.com>
2017-09-25 05:57:21 -0600

    setup Bat class with its animation

---
+++ pierre_penguin/Bat.swift
@@ -0,0 +1,43 @@
+//
+//  Bat.swift
+//  pierre_penguin
+//
+//  Created by cdrainxv on 2017-09-25.
+//  Copyright © 2017 cdrainxv. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Bat: SKSpriteNode, GameSprite {
+    var initialSize: CGSize = CGSize(width: 44, height: 24)
+    var textureAtlas: SKTextureAtlas = SKTextureAtlas(named: "Enemies")
+    var flyAnimation = SKAction()
+    
+    init() {
+        super.init(texture: nil, color: .clear, size: initialSize)
+        self.physicsBody = SKPhysicsBody(circleOfRadius: size.width / 2)
+        self.physicsBody?.affectedByGravity = false
+        
+        createAnimations()
+        self.run(flyAnimation)
+    }
+    
+    func createAnimations() {
+        let flyFrames: [SKTexture] = [
+            textureAtlas.textureNamed("bat"),
+            textureAtlas.textureNamed("bat-fly")
+        ]
+        
+        let flyAction = SKAction.animate(with: flyFrames, timePerFrame: 0.12)
+        flyAnimation = SKAction.repeatForever(flyAction)
+    }
+    
+    func onTap() {
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
