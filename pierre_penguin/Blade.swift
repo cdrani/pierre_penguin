248f7e14da50634d3ebd0f93eba5917c5c23584e
cdrainxv <cdrainxv@users.noreply.github.com>
2017-09-25 06:10:05 -0600

    setup Blade class with its animation

---
+++ pierre_penguin/Blade.swift
@@ -0,0 +1,43 @@
+//
+//  Blade.swift
+//  pierre_penguin
+//
+//  Created by cdrainxv on 2017-09-25.
+//  Copyright © 2017 cdrainxv. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Blade: SKSpriteNode, GameSprite {
+    var initialSize: CGSize = CGSize(width: 185, height: 92)
+    var textureAtlas: SKTextureAtlas = SKTextureAtlas(named: "Enemies")
+    var spinAnimation = SKAction()
+    
+    init() {
+        super.init(texture: nil, color: .clear, size: initialSize)
+        let startTexture = textureAtlas.textureNamed("blade")
+        self.physicsBody = SKPhysicsBody(texture: startTexture, size: initialSize)
+        self.physicsBody?.affectedByGravity = false
+        self.physicsBody?.isDynamic = false
+        createAnimations()
+        self.run(spinAnimation)
+    }
+    
+    func createAnimations() {
+        let spinFrames: [SKTexture] = [
+            textureAtlas.textureNamed("blade"),
+            textureAtlas.textureNamed("blade-2")
+        ]
+        let spinAction = SKAction.animate(with: spinFrames, timePerFrame: 0.07)
+        spinAnimation = SKAction.repeatForever(spinAction)
+    }
+    
+    func onTap() {
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
