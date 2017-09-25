9d12494d4f2cb94fb2298fe1289a4d0a29a146e6
cdrainxv <cdrainxv@users.noreply.github.com>
2017-09-25 06:16:52 -0600

    setup Coin class with its value(s)

---
+++ pierre_penguin/Coin.swift
@@ -0,0 +1,37 @@
+//
+//  Coin.swift
+//  pierre_penguin
+//
+//  Created by cdrainxv on 2017-09-25.
+//  Copyright © 2017 cdrainxv. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Coin: SKSpriteNode, GameSprite {
+    var initialSize: CGSize = CGSize(width: 26, height: 26)
+    var textureAtlas: SKTextureAtlas = SKTextureAtlas(named: "Environment")
+    var value = 1
+    
+    init() {
+        let bronzeTexture = textureAtlas.textureNamed("coin-bronze")
+        super.init(texture: bronzeTexture, color: .clear, size: initialSize)
+        
+        self.physicsBody = SKPhysicsBody(circleOfRadius: size.width / 2)
+        self.physicsBody?.affectedByGravity = false
+    }
+    
+    func turnToGold() {
+        self.texture = textureAtlas.textureNamed("coin-gold")
+        self.value = 5
+    }
+    
+    func onTap() {
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
