f522f0a204a8ccb30dafa87599210c6d47d779d8
cdrainxv <cdrainxv@users.noreply.github.com>
2017-09-25 05:49:00 -0600

    setup MadFly class with its animation

---
+++ pierre_penguin/MadFly.swift
@@ -0,0 +1,42 @@
+//
+//  MadFly.swift
+//  pierre_penguin
+//
+//  Created by cdrainxv on 2017-09-25.
+//  Copyright © 2017 cdrainxv. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class MadFly: SKSpriteNode, GameSprite {
+    var initialSize: CGSize = CGSize(width: 61, height: 29)
+    var textureAtlas: SKTextureAtlas = SKTextureAtlas(named: "Enemies")
+    var flyAnimation = SKAction()
+    
+    init() {
+        super.init(texture: nil, color: .clear, size: initialSize)
+        self.physicsBody = SKPhysicsBody(circleOfRadius: size.width / 2)
+        self.physicsBody?.affectedByGravity = false
+        createAnimations()
+        self.run(flyAnimation)
+    }
+    
+    func createAnimations() {
+        let flyFrames: [SKTexture] = [
+            textureAtlas.textureNamed("madfly"),
+            textureAtlas.textureNamed("madfly-fly")
+        ]
+        
+        let flyAction = SKAction.animate(with: flyFrames, timePerFrame: 0.14)
+        flyAnimation = SKAction.repeatForever(flyAction)
+    }
+    
+    func onTap() {
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
